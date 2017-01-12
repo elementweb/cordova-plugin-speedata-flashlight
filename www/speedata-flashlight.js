974d2f91e8604fa3a66693f44a1e938e36acf28a
Kostas Gliozeris <kostasmgl@gmail.com>
2017-01-12 22:20:12 +0000

    update

--- www/speedata-flashlight.js
+++
@@ -1,9 +0,0 @@
-var PLUGIN_NAME = 'SpeedataFlashlight';
-
-var SpeedataFlashlight = {
-  greet: function (name, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, PLUGIN_NAME, "greet", [name]);
-  }
-};
-
-module.exports = SpeedataFlashlight;
